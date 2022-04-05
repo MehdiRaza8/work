f6bd57c17dee9223e182f878d5591c9e1a7d744e
Mehdi Raza <razamehdi2019@gmail.com>
2022-04-04 18:17:55 -0700

    work complete

---
+++ dart.dart
@@ -0,0 +1,56 @@
+void main() {
+  print("hellow World");
+  // var mylist = [0, 'one', 'two', 'three', 'four'];
+  // mylist[3] = 'Mehdi';
+  // mylist.replaceRange(2,4,['hamza',false]);
+
+  // var list1 = [2, 7, 4, 3, 8, 9, 10, 1, 5, 6];
+  // var x = List.of(list1.reversed);
+  // print(list1);
+
+  // print(mylist);
+  // var map1 = {1: 'Mehdi', 2: 'Raza'};
+  // var map2 = {3:'Moin', 4:'Saleem'};
+  // var mapp = {}
+  //   ..addAll(map1)
+  //   ..addAll(map2);
+  // var abc = map1.putIfAbsent(1, () => 'Mango');
+  // map1.addAll({6: 'mehdii', 9: 'saeed'});
+  // var student = {
+  //   '12a': ['Mehdi', 22, 'b'],
+  //   '13b': ['raza', 33, 'c']
+  // };
+  // print(student);
+  // map1.removeWhere((key, value) => value.startsWith('M'));
+  // print(mapp);
+  // var map1 = {};
+  // print(map1.isEmpty);
+  // var map1 = {1: 'Mehdi', 2: 'Raza'};
+  // map1.forEach((key, value) {
+  //   print($key and value are :$value');
+  // });
+
+  // var list2 = ["Mehdi","Raza","Ali","hasan"];
+  // var mapp = Map.fromIterables(list1, list2);
+  // print(mapp);
+  // var abc = map1.containsKey(1);
+  // print(abc);
+  // var map1 = {1: 'Mehdi', 2: 'Raza'};
+  // map1.forEach((key, value) {print(key)
+  // })
+  // var num = 2;
+  // for (var i = 1; i < 2;) {
+  //   print(i * num);
+  // var students = ['mehdi', 'raza', 'hasan', 'ali', 'hussain'];
+  // for (var i = 0; i < students.length; i++) {
+  // print(students[i]);
+
+  // var a = 'yes';
+  // while (a == 'yes') {
+  //   print(a);
+  //   a = 'no';
+  // var abc = 2;
+  // for (var i = 0; i <= 10; i++) {
+  //   print(i * abc);
+  // }
+  }
---
+++ main.dart
@@ -0,0 +1,41 @@
+void main() {
+  // print("hellow World");
+  // var mylist = [0, 'one', 'two', 'three', 'four'];
+  // mylist[3] = 'Mehdi';
+  // mylist.replaceRange(2,4,['hamza',false]);
+
+  // var list1 = [2, 7, 4, 3, 8, 9, 10, 1, 5, 6];
+  // var x = List.of(list1.reversed);
+  // print(list1);
+
+  // print(mylist);
+  // var map1 = {1: 'Mehdi', 2: 'Raza'};
+  // var map2 = {3:'Moin', 4:'Saleem'};
+  // var mapp = {}
+  //   ..addAll(map1)
+  //   ..addAll(map2);
+  // var abc = map1.putIfAbsent(1, () => 'Mango');
+  // map1.addAll({6: 'mehdii', 9: 'saeed'});
+  // var student = {
+  //   '12a': ['Mehdi', 22, 'b'],
+  //   '13b': ['raza', 33, 'c']
+  // };
+  // print(student);
+  // map1.removeWhere((key, value) => value.startsWith('M'));
+  // print(mapp);
+  // var map1 = {};
+  // // print(map1.isEmpty);
+  // var map1 = {1: 'Mehdi', 2: 'Raza'};
+  // map1.forEach((key, value) {
+  //   print('Keys 1:$key and value are :$value');
+  // });
+  // var list2 = ["Mehdi","Raza","Ali","hasan"];
+  // var mapp = Map.fromIterables(list1, list2);
+  // print(mapp);
+  // var abc = map1.containsKey(1);
+  // print(abc);
+  var abc = 1;
+  for (a = 0; a <= 10; a++) {
+    print(a);
+  }
+}
