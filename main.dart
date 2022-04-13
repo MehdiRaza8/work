fa7f954519d3b5747d71a6b2e5c711e83698521b
Mehdi Raza <razamehdi2019@gmail.com>
2022-04-13 14:20:11 -0700

    complete

--- main.dart
+++ main.dart
@@ -1,41 +1,26 @@
+import 'dart:async';
+
 void main() {
-  // print("hellow World");
-  // var mylist = [0, 'one', 'two', 'three', 'four'];
-  // mylist[3] = 'Mehdi';
-  // mylist.replaceRange(2,4,['hamza',false]);
+  Student student1 = Student(name: "Mehdi", sec: "B");
+  student1.StudentData();
+  Student student2 = Student(name: "ALi", sec: "A");
+  student2.StudentData();
+  Student student3 = Student(name: "Hasan", sec: "C");
+  student3.StudentData();
+  Student student4 = Student(name: "HUssain", sec: "D");
+  student4.StudentData();
+  Student student5 = Student(name: "Zain", sec: "F");
+  student5.StudentData();
+  Student student6 = Student(name: "Shahzaib", sec: "H");
+  student6.StudentData();
+}
 
-  // var list1 = [2, 7, 4, 3, 8, 9, 10, 1, 5, 6];
-  // var x = List.of(list1.reversed);
-  // print(list1);
-
-  // print(mylist);
-  // var map1 = {1: 'Mehdi', 2: 'Raza'};
-  // var map2 = {3:'Moin', 4:'Saleem'};
-  // var mapp = {}
-  //   ..addAll(map1)
-  //   ..addAll(map2);
-  // var abc = map1.putIfAbsent(1, () => 'Mango');
-  // map1.addAll({6: 'mehdii', 9: 'saeed'});
-  // var student = {
-  //   '12a': ['Mehdi', 22, 'b'],
-  //   '13b': ['raza', 33, 'c']
-  // };
-  // print(student);
-  // map1.removeWhere((key, value) => value.startsWith('M'));
-  // print(mapp);
-  // var map1 = {};
-  // // print(map1.isEmpty);
-  // var map1 = {1: 'Mehdi', 2: 'Raza'};
-  // map1.forEach((key, value) {
-  //   print('Keys 1:$key and value are :$value');
-  // });
-  // var list2 = ["Mehdi","Raza","Ali","hasan"];
-  // var mapp = Map.fromIterables(list1, list2);
-  // print(mapp);
-  // var abc = map1.containsKey(1);
-  // print(abc);
-  var abc = 1;
-  for (a = 0; a <= 10; a++) {
-    print(a);
+class Student {
+  String name = "Mehdi";
+  String sec = "Raza";
+  Student({required this.name, this.sec = ""});
+  dynamic StudentData() {
+    print(name);
+    print(sec);
   }
 }
